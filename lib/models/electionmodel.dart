f6658e7a29d1143485d149a95bd4448d9f18c5d4
matifpervaizofficial <matifpervaizofficial@gmail.com>
2023-02-12 15:13:36 +0500

    models setup

---
+++ lib/models/electionmodel.dart
@@ -0,0 +1,23 @@
+import 'package:e_vote/features/ui/bloc/user_bloc/user_bloc_bloc.dart';
+import 'package:e_vote/models/candidate_model.dart';
+import 'package:e_vote/models/voter_model.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class ElectionModel extends Equatable {
+  final String adminAddress;
+  final String description;
+  final int candidateCount;
+  List<Candidate> candidates;
+  List<Voter> voters;
+  Candidate winner;
+  ElectionModel(
+      {@required this.adminAddress,
+      this.candidateCount,
+      this.candidates,
+      @required this.description,
+      this.voters,
+      this.winner});
+  List<Object> get props =>
+      [adminAddress, description, candidateCount, candidates, voters, winner];
+}
---
+++ lib/models/votermodel.dart
@@ -0,0 +1,41 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class Voter extends Equatable {
+  final int id;
+  final String name;
+  final String address;
+  final bool hasVoted;
+  final int weight;
+  final int voteTowards;
+  String delegateAddress;
+  Voter(
+      {this.id,
+      this.delegateAddress,
+      @required this.address,
+      this.hasVoted,
+      this.name,
+      this.voteTowards,
+      @required this.weight});
+
+  List<Object> get props =>
+      [address, hasVoted, name, voteTowards, delegateAddress, weight];
+
+  factory Voter.fromJson(Map<String, dynamic> json) {
+    return Voter(
+        id: json["id"],
+        delegateAddress: json["delegate"],
+        weight: json["weight"],
+        address: json["voterAddress"]);
+  }
+
+  factory Voter.profileJson(Map<String, dynamic> json, String voterAddress) {
+    return Voter(
+        address: voterAddress,
+        weight: json["weight"],
+        delegateAddress: json["delegate"],
+        name: json["name"],
+        voteTowards: json["votedTowards"],
+        id: json["id"]);
+  }
+}
